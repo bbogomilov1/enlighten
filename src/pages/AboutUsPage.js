bc61bb2bf76fb1e429cfa9ab633fceddea2fce14
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-29 00:35:35 +0300

    added enroll button on home page

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,7 +1,18 @@
 function AboutUsPage() {
   return (
     <div>
-      <p>about us page</p>
+      <p>
+        At MasterMinds, we provide engaging and interactive English classes
+        designed specifically for kids. Our experienced teachers make learning
+        English fun and exciting through games, activities, and creative
+        lessons.
+      </p>
+      <p>
+        Whether your child is a beginner or already has some English knowledge,
+        our academy offers a structured curriculum tailored to their needs. We
+        focus on building strong foundations in listening, speaking, reading,
+        and writing, ensuring steady progress and confidence.
+      </p>
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,24 @@
 import styles from "./HomePage.module.css";
 import "../static/fonts.css";
+import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <div className={styles.container}>
       <div>
         <h1>
-          Start your English learning
-          <br /> <section> journey today!</section>
+          Welcome to <br /> <section> MasterMinds Academy! </section>
         </h1>
-        <p>Enroll now and unlock new opportunities.</p>
+        <p>Unlock the power of English learning with us.</p>
+        <p>
+          Join us now on our journey today and unlock the door to endless
+          opportunities.
+        </p>
+        <div>
+          <Link to="/schedule" className={styles.enrollButton} type="button">
+            Enroll Now
+          </Link>
+        </div>
       </div>
     </div>
   );
