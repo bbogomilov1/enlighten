52f9ea3e9d125feccc6c12cadbe881160e88ee48
bbogomilov1 <bdbogomilov@gmail.com>
2023-05-30 14:47:37 +0300

    added styling in about-us section in home page

--- src/components/home/HomeAboutUs.js
+++ src/components/home/HomeAboutUs.js
@@ -6,7 +6,7 @@ function HomeAboutUs() {
       <h1>
         Learn English in an innovative <br /> and interactive way!
       </h1>
-      <section>
+      <div>
         <h2>Engage with the Best English Teachers</h2>
         <p>
           At our academy, we bring together a team of highly qualified and
@@ -14,8 +14,8 @@ function HomeAboutUs() {
           teaching, your child will receive top-notch guidance to master the
           English language.
         </p>
-      </section>
-      <section>
+      </div>
+      <div>
         <h2>Immerse in Fun and Interactive Learning</h2>
         <p>
           We believe in making English learning enjoyable and interactive for
@@ -23,8 +23,8 @@ function HomeAboutUs() {
           them in hands-on activities, games, and creative projects. Your child
           will have a blast while developing language skills.
         </p>
-      </section>
-      <section>
+      </div>
+      <div>
         <h2>Innovative Teaching Methodology</h2>
         <p>
           We follow a dynamic and innovative teaching methodology tailored to
@@ -32,8 +32,8 @@ function HomeAboutUs() {
           traditional and modern techniques, incorporating multimedia resources,
           interactive technology, and personalized learning plans.
         </p>
-      </section>
-      <section>
+      </div>
+      <div>
         <h2>Cultivate Confidence and Communication Skills</h2>
         <p>
           Communication is key! We focus on nurturing your child's confidence in
@@ -41,7 +41,7 @@ function HomeAboutUs() {
           writing exercises. Our goal is to equip them with strong communication
           skills that will empower them in their personal and academic journeys.
         </p>
-      </section>
+      </div>
     </div>
   );
 }
