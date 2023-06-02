888bc08b4a26346e398153d54dd2f373c77b375e
bbogomilov1 <bdbogomilov@gmail.com>
2023-06-02 19:33:22 +0300

    added mockup blob on home-page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,7 @@ import "../static/fonts.css";
 import HomeTitleSection from "../components/home/HomeTitleSection";
 import HomeAboutUs from "../components/home/HomeAboutUs";
 import LogoProvider from "../components/Logo";
+import styles from "./HomePage.module.css";
 
 function HomePage() {
   return (
