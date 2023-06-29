c91e4173e9de77a3aa0aa658b9346265e3a37a91
bbogomilov1 <bdbogomilov@gmail.com>
2023-06-30 00:02:58 +0300

    added blob in our-mission section

--- src/components/about-us/OurMission.js
+++ src/components/about-us/OurMission.js
@@ -31,6 +31,17 @@ function OurMission() {
           <img src={rocketImage} alt="rocket" className={styles.teacherImage} />
         </div>
       </div>
+      <svg
+        viewBox="0 0 200 200"
+        xmlns="http://www.w3.org/2000/svg"
+        className={styles.blobOne}
+      >
+        <path
+          fill="rgb(186 218 246)"
+          d="M37.1,-14.8C44.3,10,43.7,34.6,26.7,50.1C9.7,65.6,-23.6,72,-46.6,56.9C-69.6,41.9,-82.3,5.5,-72.8,-22.4C-63.3,-50.4,-31.7,-69.9,-8.3,-67.2C15,-64.5,30,-39.6,37.1,-14.8Z"
+          transform="translate(100 100)"
+        />
+      </svg>
     </div>
   );
 }
