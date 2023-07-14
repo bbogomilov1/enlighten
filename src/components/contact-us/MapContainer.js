6e23cdd64e21c8f1e1543b8899588a1e93a4fe4b
bbogomilov1 <bdbogomilov@gmail.com>
2023-07-14 08:27:38 +0300

    added marker on map

--- src/components/contact-us/MapContainer.js
+++ src/components/contact-us/MapContainer.js
@@ -1,15 +1,37 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+// const customIcon = L.icon({
+//   iconUrl: icon,
+//   shadowUrl: iconShadow,
+//   iconSize: [25, 41], // Adjust the icon size if needed
+//   iconAnchor: [12, 41], // Adjust the icon anchor if needed
+//   popupAnchor: [1, -34], // Adjust the popup anchor if needed
+//   // Define custom marker options, including the color
+//   iconColor: "red", // Change the color to your desired value
+// });
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: icon,
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
 
 function Map() {
+  const position = [42.66929183810461, 23.29754963197396];
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={position}
+      zoom={16}
       style={{ height: "400px", width: "100%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={position} />
     </MapContainer>
   );
 }
