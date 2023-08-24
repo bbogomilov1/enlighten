ffdf890db2ec7e526825bcf6b543bd8e607da02b
bbogomilov1 <bdbogomilov@gmail.com>
2023-08-24 17:57:42 +0300

    styled our mission section in about us

--- src/components/about-us/OurMission.js
+++ src/components/about-us/OurMission.js
@@ -8,12 +8,12 @@ function OurMission() {
       <div className={styles.description}>
         <div className={styles.paragraphsContainer}>
           <p>
-            At MasterMinds Kids Academy, our mission is to provide a nurturing
-            and engaging learning environment where children can develop a
-            strong foundation in English language skills. We believe that every
-            child has the potential to excel and grow, and we are dedicated to
-            unlocking their linguistic abilities through interactive and
-            immersive teaching methods.
+            At EnlightEN, our mission is to provide a nurturing and engaging
+            learning environment where children can develop a strong foundation
+            in English language skills. We believe that every child has the
+            potential to excel and grow, and we are dedicated to unlocking their
+            linguistic abilities through interactive and immersive teaching
+            methods.
           </p>
           <p>
             Our experienced and passionate educators strive to create a
@@ -37,7 +37,7 @@ function OurMission() {
         className={styles.blobOne}
       >
         <path
-          fill="rgb(186 218 246)"
+          fill="#c28e00"
           d="M37.1,-14.8C44.3,10,43.7,34.6,26.7,50.1C9.7,65.6,-23.6,72,-46.6,56.9C-69.6,41.9,-82.3,5.5,-72.8,-22.4C-63.3,-50.4,-31.7,-69.9,-8.3,-67.2C15,-64.5,30,-39.6,37.1,-14.8Z"
           transform="translate(100 100)"
         />
--- src/components/about-us/ThaAcademy.js
+++ src/components/about-us/ThaAcademy.js
@@ -4,17 +4,6 @@ import kidsImage from "../../static/1669412766.jpg";
 function TheAcademy() {
   return (
     <div className={styles.container}>
-      {/* <svg
-        viewBox="0 0 200 200"
-        xmlns="http://www.w3.org/2000/svg"
-        className={styles.blobOne}
-      >
-        <path
-          fill="#f9e888"
-          d="M34,-38.2C48.9,-35.9,69.4,-32.5,73.6,-23.1C77.8,-13.7,65.7,1.9,55.6,13.6C45.6,25.2,37.7,33.1,28.7,38.2C19.8,43.4,9.9,45.9,-3,50C-15.9,54.2,-31.8,60,-40.7,54.8C-49.6,49.6,-51.5,33.5,-54.7,18.9C-58,4.3,-62.6,-8.8,-62,-23.1C-61.4,-37.4,-55.6,-52.9,-44.4,-56.4C-33.1,-59.9,-16.6,-51.4,-3.5,-46.6C9.5,-41.7,19,-40.4,34,-38.2Z"
-          transform="translate(100 100)"
-        />
-      </svg> */}
       <h1>The Academy</h1>
       <div className={styles.description}>
         <div>
@@ -22,7 +11,7 @@ function TheAcademy() {
         </div>
         <div className={styles.textDescription}>
           <p>
-            At MasterMinds, we provide engaging and interactive English classes
+            At EnlightEN, we provide engaging and interactive English classes
             designed specifically for kids. Our experienced teachers make
             learning English fun and exciting through games, activities, and
             creative lessons.
