575d022d137bc9d021195fadeeb1e94f914049c7
bbogomilov1 <bdbogomilov@gmail.com>
2023-08-29 23:37:58 +0300

    added inquiry form

--- src/components/contact-us/Inquiry.js
+++ src/components/contact-us/Inquiry.js
@@ -27,32 +27,42 @@ function Inquiry() {
       <div className={styles.listContainer}>
         <h2>{t("Make an Inquiry")}</h2>
         <p>{t("Want to learn more...")}</p>
-        <form action="#" className={styles.inquiryForm} onSubmit={handleSubmit}>
-          <div className={styles.inputGroup}>
-            <input
-              type="text"
-              name="from_name"
-              placeholder="Full Name *"
+        <div className={styles.formContainer}>
+          <form
+            action="#"
+            className={styles.inquiryForm}
+            onSubmit={handleSubmit}
+          >
+            <div className={styles.inputGroup}>
+              <input
+                type="text"
+                name="from_name"
+                placeholder="Full Name *"
+                required
+              />
+            </div>
+            <div className={styles.inquiryGroup}>
+              <input
+                type="email"
+                name="from_email"
+                placeholder="Email *"
+                required
+              />
+              <input
+                type="tel"
+                name="from_phone"
+                placeholder="Phone Number *"
+                required
+              />
+            </div>
+            <textarea
+              name="from_message"
+              placeholder="Your Message"
               required
-            />
-          </div>
-          <div className={styles.inquiryGroup}>
-            <input type="email" name="email" placeholder="Email *" required />
-            <input
-              type="tel"
-              name="phone"
-              placeholder="Phone Number *"
-              required
-            />
-          </div>
-          <input type="text" name="subject" placeholder="Subject *" required />
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            required
-          ></textarea>
-          <button type="submit">Submit</button>
-        </form>
+            ></textarea>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   );
