3d006c0909909d755e97e91edb0720431f140535
bbogomilov1 <bdbogomilov@gmail.com>
2023-09-11 21:54:43 +0300

    added worldready course

--- src/pages/CoursesPage.js
+++ src/pages/CoursesPage.js
@@ -1,10 +1,12 @@
 import styles from "./CoursesPage.module.css";
 import ConcentrateCourse from "../components/courses/ConcentrateCourse";
+import WorldReadyCourse from "../components/courses/WorldReadyCourse";
 
 function CoursesPage() {
   return (
     <div className={styles.container}>
       <ConcentrateCourse />
+      <WorldReadyCourse />
     </div>
   );
 }
