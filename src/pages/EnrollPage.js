6d78fab9323857add24b96c131707064272145ae
bbogomilov1 <bdbogomilov@gmail.com>
2023-11-03 14:25:57 +0200

    added style in enroll page

--- src/pages/EnrollPage.js
+++ src/pages/EnrollPage.js
@@ -1,9 +1,11 @@
 import styles from "./EnrollPage.module.css";
 import { useTranslation } from "react-i18next";
 import emailjs from "emailjs-com";
+import { useState } from "react";
 
 function EnrollPage() {
   const { t } = useTranslation();
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -22,13 +24,16 @@ function EnrollPage() {
     }
   };
 
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <div>
       <div className={styles.container}>
         <div className={styles.enrollContainer}>
           <div className={styles.listContainer}>
             <h2 className={styles.title}>· ЗАПИШИ СЕ ЗА ЧАС ·</h2>
-            {/* <p>{t("Want to learn more...")}</p> */}
             <div className={styles.formContainer}>
               <form
                 action="#"
@@ -37,7 +42,7 @@ function EnrollPage() {
               >
                 <div className={styles.inputGroup}>
                   <p className={styles.inputTitle}>
-                    Имена на родител/настойник
+                    Имена на родител/настойник:
                   </p>
                   <input
                     type="text"
@@ -48,7 +53,7 @@ function EnrollPage() {
                 </div>
 
                 <div className={styles.inputGroup}>
-                  <p className={styles.inputTitle}>Имена на дете</p>
+                  <p className={styles.inputTitle}>Имена на дете:</p>
                   <input
                     type="text"
                     name="child_name"
@@ -58,7 +63,7 @@ function EnrollPage() {
                 </div>
 
                 <div className={styles.inputGroup}>
-                  <p className={styles.inputTitle}>Телефон за връзка</p>
+                  <p className={styles.inputTitle}>Телефон за връзка:</p>
                   <input
                     type="tel"
                     name="phone"
@@ -67,63 +72,134 @@ function EnrollPage() {
                   />
                 </div>
 
-                <div className={styles.radioGroup}>
-                  <p className={styles.inputTitle}>Избери курс</p>
-                  <input
-                    type="radio"
-                    id="concentrate"
-                    name="course"
-                    value="concENtrate"
-                  />
-                    <label for="concentrate">concENtrate</label>
-                  <br />
-                  <input
-                    type="radio"
-                    id="worldready"
-                    name="course"
-                    value="WorldReady"
-                  />
-                    <label for="worldready">WorldReady</label>
-                  <br />
-                  <input
-                    type="radio"
-                    id="smartart"
-                    name="course"
-                    value="SmartArt"
-                  />
-                    <label for="smartart">SmartArt</label>
-                  <br />
-                  <input
-                    type="radio"
-                    id="kids"
-                    name="course"
-                    value="ENlightEN Kids"
-                  />
-                    <label for="kids">ENlightEN Kids</label>
-                  <br />
-                  <input
-                    type="radio"
-                    id="private"
-                    name="course"
-                    value="ENlightEN Private"
-                  />
-                    <label for="private">ENlightEN Private</label>
-                  <br />
-                  <input
-                    type="radio"
-                    id="presenters"
-                    name="course"
-                    value="Young PresENters"
-                  />
-                    <label for="presenters">Young PresENters</label>
-                  <br />
-                </div>
+                <div className={styles.radioGroupContainer}>
+                  <div className={styles.radioGroup}>
+                    <p className={styles.inputTitle}>Избери курс:</p>
+                    <div className={styles.radioGroupInputs}>
+                      <input
+                        type="radio"
+                        id="concentrate"
+                        name="course"
+                        value="concENtrate"
+                        checked={selectedOption === "concENtrate"}
+                        onChange={handleOptionChange}
+                      />
+                        <label htmlFor="concentrate">concENtrate</label>
+                      {/* {selectedOption === "concENtrate" && (
+                        <div>
+                          <input
+                            type="radio"
+                            name="sub-option1"
+                            value="sub-option1-1"
+                          />
+                          <label htmlFor="sub-option1-1">Sub-option 1.1</label>
+                          <br />
+                          <input
+                            type="radio"
+                            name="sub-option1"
+                            value="sub-option1-2"
+                          />
+                          <label htmlFor="sub-option1-2">Sub-option 1.2</label>
+                        </div>
+                      )} */}
+                      <br />
+                      <input
+                        type="radio"
+                        id="worldready"
+                        name="course"
+                        value="WorldReady"
+                      />
+                        <label htmlFor="worldready">WorldReady</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="smartart"
+                        name="course"
+                        value="SmartArt"
+                      />
+                        <label htmlFor="smartart">SmartArt</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="kids"
+                        name="course"
+                        value="ENlightEN Kids"
+                      />
+                        <label htmlFor="kids">ENlightEN Kids</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="private"
+                        name="course"
+                        value="ENlightEN Private"
+                      />
+                        <label htmlFor="private">ENlightEN Private</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="presenters"
+                        name="course"
+                        value="Young PresENters"
+                      />
+                        <label htmlFor="presenters">Young PresENters</label>
+                      <br />
+                    </div>
+                  </div>
 
-                {/* <textarea
-                  name="from_message"
-                  placeholder={t("Your Message") + " *"}
-                  required
-                ></textarea> */}
+                  {/* <div className={styles.radioGroup}>
+                    <p className={styles.inputTitle}>Избери ден и час:</p>
+                    <div className={styles.radioGroupInputs}>
+                      <input
+                        type="radio"
+                        id="concentrate"
+                        name="course"
+                        value="concENtrate"
+                      />
+                        <label htmlFor="concentrate">concENtrate</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="worldready"
+                        name="course"
+                        value="WorldReady"
+                      />
+                        <label htmlFor="worldready">WorldReady</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="smartart"
+                        name="course"
+                        value="SmartArt"
+                      />
+                        <label htmlFor="smartart">SmartArt</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="kids"
+                        name="course"
+                        value="ENlightEN Kids"
+                      />
+                        <label htmlFor="kids">ENlightEN Kids</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="private"
+                        name="course"
+                        value="ENlightEN Private"
+                      />
+                        <label htmlFor="private">ENlightEN Private</label>
+                      <br />
+                      <input
+                        type="radio"
+                        id="presenters"
+                        name="course"
+                        value="Young PresENters"
+                      />
+                        <label htmlFor="presenters">Young PresENters</label>
+                      <br />
+                    </div>
+                  </div> */}
+                </div>
 
                 <div className={styles.formButtonContainer}>
                   <button type="submit" className={styles.formButton}>
