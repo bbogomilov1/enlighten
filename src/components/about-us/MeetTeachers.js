511fc1e01d15c81369f2021768060749b01a7802
Bogomil <bdbogomilov@gmail.com>
2024-08-16 22:49:55 +0300

    changed teachers pic

--- src/components/about-us/MeetTeachers.js
+++ src/components/about-us/MeetTeachers.js
@@ -1,5 +1,5 @@
 import styles from "./MeetTeachers.module.css";
-import laritaImage from "../../static/311373822_5340375706012360_6099292886436765504_n.jpg";
+import laritaImage from "../../static/IMG_5948.jpg";
 // import betinaImage from "../../static/Screenshot 2023-06-01 202659.jpg";
 import { useTranslation } from "react-i18next";
 import { useEffect, useRef, useState } from "react";
